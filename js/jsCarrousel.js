a8336c59291d31da6d46d41abdd40983813bf625
Fabrizio Arias <fabrisal@live.com.ar>
2024-05-25 01:31:48 -0300

    Carrousel
    Se creo el carrousel con sus estilos, y se cambiaron un par de estilos mas del home

---
+++ js/jsCarrousel.js
@@ -0,0 +1,44 @@
+let imagenes = ["imagenes/carrousel/noria2.jpg",
+                "imagenes/carrousel/centroia2.jpg",
+                "imagenes/carrousel/suria2.jpg"                
+                ];
+
+document.ImgCarru.src = imagenes[0];
+let contador = 0;
+
+
+
+function moverDer(){
+    contador++;
+    if(contador > imagenes.length -1){
+        contador = 0;
+    }
+    document.ImgCarru.src = imagenes[contador];
+    
+}
+
+function moverIzq(){
+    contador--;
+    if(contador < 0){
+        contador = imagenes.length -1;
+    }
+    document.ImgCarru.src = imagenes[contador];
+}
+/* ------------ intervalo para que se vaya pasando sola ------------ */
+
+let intervalo = setInterval(moverDer,4000); 
+
+function limpiarIntervaloDer(){
+    clearInterval(intervalo);
+    moverDer();
+    intervalo = setInterval(moverDer(),4000);
+    console.log("der");
+}
+
+function limpiarIntervaloIzq(){
+    clearTimeout(intervalo);
+    moverIzq();
+    intervalo = setInterval(moverIzq(),4000);
+    
+    
+}
