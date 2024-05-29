7c62acabd28dad604ab00ae2386afeedc5a6c1f4
Fabrizio Arias <fabrisal@live.com.ar>
2024-05-29 00:30:06 -0300

    ultimos detalles
    prácticamente lista para entregar!

--- js/script.js
+++ js/script.js
@@ -15,26 +15,26 @@ function validar() {
 
 
   if (nbr.length == 0) {
-    err.push("por favor ingrese un nombre");
-  } else if (nbr.trim.length >= 30) {
-    err.push("nombre muy largo");
+    err.push("Por favor, ingrese un nombre");
+  } else if (nbr.length >= 30) {
+    err.push("El nombre es muy largo");
   }
 
   let crr = correo.value.trim();
 
   if (crr.length == 0) {
-    err.push("por favor ingrese un correo")
+    err.push("Por favor, ingrese un correo")
   } else if (!exreg.test(crr)) {
     err.push("Ingrese un mail valido")
-    err.push("ej: usr@mail.com")
+    err.push("Ej: usuario@mail.com")
   }
 
   let nmr = celular.value;
 
   if (nmr.length == 0) {
-    err.push("Por favor ingrese un celular");
-  } else if (nmr.trim.length >= 10) {
-    err.push("Celular muy largo");
+    err.push("Por favor, ingrese un celular");
+  } else if (nmr.length >= 10) {
+    err.push("El celular es muy largo");
   }
 
   document.getElementById("errores").innerHTML = "";
@@ -49,7 +49,7 @@ function validar() {
   document.getElementById("info").innerHTML ="";
   if (err.length == 0) {
 
-    document.getElementById("info").innerHTML = `Hola ${nbr}, nos comunicaremos al ${nmr} ,o le enviaremos un mail al ${crr}`;
+    document.getElementById("info").innerHTML = `¡Hola ${nbr}! Nos comunicaremos al ${nmr}, o le enviaremos un mail al ${crr}`;
 
     
   }
